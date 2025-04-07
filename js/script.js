28594b0904873c7ef2ea12af18c005b358a137e7
SENE0711 <ethan20071122@outlook.com>
2025-04-08 10:55:11 +1200

    cover image switch every 5 second

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Script for the cover image that changes every 5 seconds
+var activateCoverId = 1;
+
+var nextCoverId = 1;
+
+setInterval(function() {
+    nextCoverId = nextCoverId + 1;
+
+    if (nextCoverId < 7) {
+
+        document.getElementById("c"+activateCoverId).classList.replace("visible", "hidden");
+        document.getElementById("c"+nextCoverId).classList.replace("hidden", "visible");
+
+        activateCoverId = nextCoverId;
+
+    } else {
+
+        document.getElementById("c"+activateCoverId).classList.replace("visible", "hidden");
+        document.getElementById("c"+nextCoverId).classList.replace("hidden", "visible");
+
+        activateCoverId = 7;
+        nextCoverId = 0;
+
+    }
+}, 5000);
